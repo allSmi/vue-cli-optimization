f648e2dfcb6fd3ec6c7d3d72afdcd461e081d604
__Steel笑口 <kewangxunqiu@163.com>
2019-04-17 00:31:23 +0800

    优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button>123</van-button>
+    <van-cell>234</van-cell>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import { Button, Cell } from 'vant'
+Vue.use(Button).use(Cell)
 export default {
   name: 'home',
   components: {
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,33 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 module.exports = {
   configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
+    plugins: [new BundleAnalyzerPlugin()],
+    externals: {
+      'vue': 'Vue',
+      'vue-router': 'VueRouter'
+    },
+    optimization: {
+      runtimeChunk: {
+        name: 'manifest'
+      },
+      splitChunks: {
+        chunks: 'async',
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        name: false,
+        cacheGroups: {
+          vendor: {
+            name: 'vendor',
+            // chunks 有三个可选值，”initial”, “async” 和 “all”. 分别对应优化时只选择初始的chunks，所需要的chunks 还是所有chunk
+            chunks: 'initial',
+            priority: -10,
+            reuseExistingChunk: false,
+            test: /[\\/]node_modules[\\/]/
+          }
+        }
+      }
+    }
   }
+
 }
