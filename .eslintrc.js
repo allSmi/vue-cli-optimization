f30a3d614f341fba2662ac6574aac47e3e1f3ef4
hanzheng <kewangxunqiu@163.com>
2019-04-17 21:08:58 +0800

    优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-multiple-empty-lines': [0, { max: 2 }],
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,5 +2,4 @@ import 'amfe-flexible';
 import 'axios';
 import './index.scss';
 
-console.log('1111112222');
 window.aaa = 1111;
